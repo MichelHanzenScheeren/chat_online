231303a188c59111dd0459f732dd50449a1263ef
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-04-12 20:41:58 -0300

    Configurações relacionadas a conexão da aplicação com o firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:chatonline/app/myApp.dart';
 
-void main () {
+void main() {
   runApp(MyApp());
 }
